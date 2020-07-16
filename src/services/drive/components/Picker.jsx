a4e6f52ae2570727c98a98ee1aef6defc9f576a0
Andrew Ligon <andrew.ligon@nextthought.com>
2020-07-16 13:51:03 -0500

    style the drive picker buttons

--- src/services/drive/components/Picker.jsx
+++ src/services/drive/components/Picker.jsx
@@ -1,24 +1,85 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames/bind';
 import {scoped} from '@nti/lib-locale';
-import {Text, Loading, Button} from '@nti/web-commons';
+import {ExternalLibraryManager} from '@nti/web-client';
+import {Text, Loading, Button, HOC} from '@nti/web-commons';
 
 import {Authorize} from '../../google';
 
+import Styles from './Picker.css';
+
+const cx = classnames.bind(Styles);
 const t = scoped('integrations.drive.components.Picker', {
-	launch: 'Open Google Picker'
+	launch: 'Open Google Picker',
+	noSelected: 'No Document Selected'
 });
+
+const GAPISource = 'https://apis.google.com/js/api.js';
+const GAPIDevKey = 'AIzaSyBntrVvY4b0pY9u4vSf_54hnDdt-eYK_H4';
+const GAPIAppId = '918146062510';
 
 const AuthScopes = ['https://www.googleapis.com/auth/drive.file'];
 
+function loadPicker () {
+	if (!loadPicker.ref) {
+		const load = async () => {
+			await ExternalLibraryManager.injectScript(GAPISource, 'gapi');
+			await new Promise((fulfill) => global.gapi.load('picker', {callback: fulfill}));
+
+			if (!global.google?.picker) {
+				throw new Error('Unable to load picker');
+			}
+
+			return global.google.picker;
+		};
+
+		loadPicker.ref = load();
+	}
+
+	return loadPicker.ref;
+}
+
+async function showPicker (authToken) {
+	const picker = await loadPicker();
+
+	return new Promise((fulfill, reject) => {
+		try {
+			const pickerView = new picker.PickerBuilder()
+				.enableFeature(picker.Feature.NAV_HIDDEN)
+				.setOAuthToken(authToken)
+				.setDeveloperKey(GAPIDevKey)
+				.setAppId(GAPIAppId)
+				.addView(new picker.View(picker.ViewId.DOCS))
+				.addView(new picker.DocsUploadView())
+				.setCallback((data) => {
+					if (data.action === picker.Action.PICKED) {
+						fulfill(data.docs);
+					} else if (data.action === picker.Action.CANCEL) {
+						fulfill(null);
+					}
+				})
+				.build();
+
+			pickerView.setVisible(true);
+
+		} catch (e) {
+			reject(e);
+		}
+	});
+}
+
+GoogleDrivePicker.Bar = HOC.Variant(GoogleDrivePicker, {className: cx('bar')});
 GoogleDrivePicker.propTypes = {
+	className: PropTypes.string,
+
 	value: PropTypes.object,
 	onChange: PropTypes.func,
 	onError: PropTypes.func,
 
 	autoLaunch: PropTypes.bool
 };
-export default function GoogleDrivePicker ({value, onChange, onError, autoLaunch}) {
+export default function GoogleDrivePicker ({className, value, onChange, onError, autoLaunch}) {
 	const [authToken, setAuthToken] = React.useState(null);
 	const [open, setOpen] = React.useState(autoLaunch);
 
@@ -26,24 +87,63 @@ export default function GoogleDrivePicker ({value, onChange, onError, autoLaunch
 		setAuthToken(token);
 	};
 
-	const onAuthFail = () => {
-		debugger;
+	const onAuthFail = (...args) => {
+		onError?.(...args);
+		setOpen(false);
 	};
+
+	const onAuthCancel = () => setOpen(false);
 
 	React.useEffect(() => {
 		if (!open || !authToken) { return; }
 
-		alert('Show Google Picker');
+		let unmounted = false;
+
+		const pick = async () => {
+			try {
+				const docs = await showPicker(authToken);
+
+				if (!unmounted) {
+					onChange?.(docs);
+					setOpen(false);
+				}
+			} catch (e) {
+				if (!unmounted) {
+					onError?.(e);
+					setOpen(false);
+				}
+			}
+		};
+
+		pick();
+		return () => unmounted = true;
 	}, [authToken, open]);
 
 	return (
-		<div>
+		<div className={cx('drive-picker', className)}>
 			{open && !authToken && (
-				<Authorize onAuthorized={onAuth} onFailure={onAuthFail} scopes={AuthScopes}/>
+				<Authorize
+					onAuthorized={onAuth}
+					onFailure={onAuthFail}
+					onCancel={onAuthCancel}
+					scopes={AuthScopes}
+				/>
 			)}
-
-			<Button onClick={open ? null : (() => setOpen(true))}>
-				{open && (<Loading.Spinner />)}
+			{value && (
+				<a href={value.url} className={cx('document')} target="_blank" rel="noopener noreferrer">
+					{value.iconUrl && (<img src={value.iconUrl} />)}
+					<Text.Base className={cx('name')}>
+						{value.name}
+					</Text.Base>
+				</a>
+			)}
+			{!value && (
+				<Text.Base className={cx('no-selection')}>
+					{t('noSelected')}
+				</Text.Base>
+			)}
+			<Button onClick={open ? null : (() => setOpen(true))} className={cx('launch')}>
+				{open && (<Loading.Spinner white size="16px" />)}
 				{!open && (
 					<Text.Base>
 						{t('launch')}
--- src/services/google/components/Authorize.jsx
+++ src/services/google/components/Authorize.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Storage from '@nti/web-storage';
+import Storage from '@nti/web-storage';//eslint-disable-line
 import {Hooks} from '@nti/web-commons';
 
 const SuccessKey = 'google-auth-success';
@@ -56,11 +56,11 @@ function usePostMessage (onMessage) {
 }
 
 
-function useAccessToken (scopes) {
+function useAccessToken (scopes, {onCancel}) {
 	const forceUpdate = Hooks.useForceUpdate();
 
 	const scopesId = getScopesId(scopes);
-	const active = Scopes[scopesId] = Scopes[scopesId] || ({token: null, error: null, windowActive: false});
+	const active = Scopes[scopesId] = Scopes[scopesId] || ({token: null, error: null, windowActive: false, windowPoll: null});
 
 	usePostMessage((e) => {
 		const {data: eventData} = e;
@@ -69,14 +69,21 @@ function useAccessToken (scopes) {
 		if (!data || !HandleMessage[data.key]) { return; }
 
 		const hashData = parseHash(hash);
+		const cleanupWindow = () => {
+			active.windowActive = false;
+			active.canceled = false;
+			clearInterval(active.windowPoll);
+		};
 
 		if (data.key === SuccessKey) {
+			cleanupWindow();
 			active.token = Storage.encodeExpiryValue(
 				hashData['access_token'],
 				Date.now() + (parseInt(hashData['expires_in'], 10) * 1000)
 			);
 			forceUpdate();
 		} else if (data.key === FailureKey) {
+			cleanupWindow();
 			active.error = hashData.error;
 			forceUpdate();
 		}
@@ -84,16 +91,29 @@ function useAccessToken (scopes) {
 
 	React.useEffect(() => {
 		if (!active.token && !active.error && !active.windowActive) {
-			active.windowActive = true;
 
 			const link = getAuthLink(scopes);
-			global.open?.(link, 'google-auth-window', 'menubar=no,titlebar=no,toolbar=no,width=800,height=600');
+			const win = global.open?.(link, 'google-auth-window', 'menubar=no,titlebar=no,toolbar=no,width=800,height=600');
+			
+			active.windowActive = true;
+			clearInterval(active.windowPoll);
+			active.windowPoll = setInterval(() => {
+				if (win.closed) {
+					active.windowActive = false;
+					clearInterval(active.windowPoll);
+
+					if (!active.token && !active.error) {
+						onCancel?.();
+					}
+				}
+			}, 500);
 		}
 	}, [active]);
 
 	return {
 		token: Storage.decodeExpiryValue(active.token),
-		error: active.error
+		error: active.error,
+		canceled: active.canceled
 	};
 }
 
@@ -101,14 +121,15 @@ function useAccessToken (scopes) {
 GoogleAuth.propTypes = {
 	onAuthorized: PropTypes.func,
 	onFailure: PropTypes.func,
+	onCancel: PropTypes.func,
 	scopes: PropTypes.array
 };
-export default function GoogleAuth ({onAuthorized, onFailure, scopes}) {
-	const {token, error} = useAccessToken(scopes);
+export default function GoogleAuth ({onAuthorized, onFailure, onCancel, scopes}) {
+	const {token, error} = useAccessToken(scopes, {onCancel});
 
 	React.useEffect(() => {
-		if (token) { onAuthorized(token); }
-		else if (error) { onFailure(error); }
+		if (token) { onAuthorized?.(token); }
+		else if (error) { onFailure?.(error); }
 	}, [token, error, onAuthorized, onFailure]);
 
 	return null;
