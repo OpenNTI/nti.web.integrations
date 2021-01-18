0a230a1a3ff973332dc2c36c8b29f8423a7f2624
Andrew Ligon <andrew.ligon@nextthought.com>
2021-01-18 17:45:43 -0600

    check point for connecting and disconnecting credly

---
+++ src/services/credly/window/Connect.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {scoped} from '@nti/lib-locale';
+import {Form, Button, Text} from '@nti/web-commons';
+
+import Styles from './Connect.css';
+
+const t = scoped('integrations.services.credly.window.Connect', {
+	title: 'To Connect to Credly Acclaim, add your Authorization Token',
+	placeholder: 'Authorization Token'
+});
+
+CredlyAcclainConnectForm.propTypes = {
+	service: PropTypes.shape({
+		connect: PropTypes.func
+	})
+};
+export default function CredlyAcclainConnectForm ({service}) {
+	const [saving, setSaving] = React.useState(false);
+
+	const submitForm = React.useCallback(({json}) => service.connect(json.authorizationToken), [service]);
+
+	return (
+		<div className={Styles.connectForm}>
+			<Text.Base className={Styles.title}>{t('title')}</Text.Base>
+			<Form onSubmit={submitForm} autoComplete="off">
+				<Form.Input.Text className={Styles.tokenInput} name="authorizationToken" placeholder={t('placeholder')} fill />
+				<Button as={Form.SubmitButton} className={Styles.submit} rounded>
+					Add Token
+				</Button>
+			</Form>
+		</div>
+	);
+}
--- src/services/credly/window/View.jsx
+++ src/services/credly/window/View.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {scoped} from '@nti/lib-locale';
+import {Prompt, Text, Hooks} from '@nti/web-commons';
 
-import BaseWindow from '../../components/BaseWindow';
+import Description from '../../components/Description';
+import WindowContents from '../../components/WindowContents';
+import Unavailable from '../../components/Unavailable';
 import {Logo} from '../assets';
+
+import Connect from './Connect';
+import Disconnect from './Disconnect';
+
+const {useChanges} = Hooks;
 
 const Link = 'https://info.credly.com/';
 
 const t = scoped('integrations.services.credly.window.View', {
-	title: 'Connect to Credly',
+	title: 'Connect to Credly Acclaim',
 	description: 'Award credentials for course completion through Credly',
 	unavailable: {
 		title: 'Connecting to Credly is currently unavailable.',
@@ -15,8 +24,34 @@ const t = scoped('integrations.services.credly.window.View', {
 	}
 });
 
-export default function CredlyConnectWindow (props) {
+CredlyConnectWindow.propTypes = {
+	service: PropTypes.shape({
+		isEnabled: PropTypes.func,
+		isConnected: PropTypes.func
+	}),
+	doClose: PropTypes.func
+};
+export default function CredlyConnectWindow ({service, doClose}) {
+	useChanges(service);
+
+	let content = null;
+
+	if (!service.isEnabled()) {
+		content = (<Unavailable service={service} title={t('unavailable.title')} supportSubject={t('unavailable.supportSubject')} />);
+	} else if (service.isConnected()) {
+		content = (<Disconnect service={service} doClose={doClose} />);
+	} else {
+		content = (<Connect service={service} doClose={doClose} />);
+	}
+
 	return (
-		<BaseWindow {...props} logo={Logo} link={Link} getString={t} />
+		<Prompt.BaseWindow doClose={doClose} title={t('title')}>
+			<Description logo={Logo} link={Link}>
+				<Text.Base>{t('description')}</Text.Base>
+			</Description>
+			<WindowContents>
+				{content}
+			</WindowContents>
+		</Prompt.BaseWindow>
 	);
 }
