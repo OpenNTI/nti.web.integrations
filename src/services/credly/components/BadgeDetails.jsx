6d82fe1c7afc87aabcdb340eb994798f678a8cec
Andrew Ligon <andrew.ligon@nextthought.com>
2021-01-26 11:54:02 -0600

    add more badge details

--- src/services/credly/components/BadgeDetails.jsx
+++ src/services/credly/components/BadgeDetails.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import {scoped} from '@nti/lib-locale';
 import {Text, StandardUI} from '@nti/web-commons';
 
 import Badge from './Badge';
 
 const {Prompt} = StandardUI;
+
+const t = scoped('integrations.services.credly.components.BadgeDetails', {
+	issuedBy: 'Issued By',
+	details: 'Details',
+	page: 'Page'
+});
 
 const styles = css`
 	.container {
@@ -34,6 +41,22 @@ const styles = css`
 		font-size: 0.875rem;
 	}
 
+	.meta {
+		list-style: none;
+		padding: 0;
+		margin: 0 0 0.625rem;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		flex-wrap: wrap;
+	}
+
+	.meta li {
+		flex: 0 0 auto;
+		width: 50%;
+		min-width: 100px;
+	}
+
 	.actions {
 		display: flex;
 		align-items: center;
@@ -44,6 +67,23 @@ const styles = css`
 			margin-right: 0.25rem;
 		}
 	}
+`;
+
+const MetaLabel = styled(Text.Base)`
+	display: block;
+	font-size: 0.625rem;
+	font-weight: 700;
+	text-transform: uppercase;
+	color: var(--tertiary-grey);
+`;
+
+const MetaValue = styled(Text.Base)`
+	display: block;
+	font-size: 0.875rem;
+	max-width: 200px;
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
 `;
 
 BadgeDetailsDialog.propTypes = {
@@ -64,7 +104,9 @@ BadgeDetails.propTypes = {
 	className: PropTypes.string,
 	badge: PropTypes.shape({
 		name: PropTypes.string,
-		description: PropTypes.string
+		description: PropTypes.string,
+		organizationName: PropTypes.string,
+		badgeURL: PropTypes.string
 	}),
 
 	hideImage: PropTypes.bool,
@@ -74,6 +116,7 @@ BadgeDetails.propTypes = {
 };
 export default function BadgeDetails ({className, badge, hideImage, actions}) {
 	const hasActions = actions && actions.length > 0;
+	const hasMeta = badge.badgeURL || badge.organizationName;
 
 	return (
 		<div className={cx(styles.container, className)}>
@@ -83,6 +126,26 @@ export default function BadgeDetails ({className, badge, hideImage, actions}) {
 				)}
 				<div className={styles.info}>
 					<Text.Base className={styles.name}>{badge.name}</Text.Base>
+					{hasMeta && (
+						<ul className={styles.meta}>
+							{badge.organizationName && (
+								<li>
+									<MetaLabel>{t('issuedBy')}</MetaLabel>
+									<MetaValue>{badge.organizationName}</MetaValue>
+								</li>
+							)}
+							{badge.badgeURL && (
+								<li>
+									<MetaLabel>{t('details')}</MetaLabel>
+									<MetaValue>
+										<a href={badge.badgeURL} ref="noopener noreferrer">
+											{t('page')}
+										</a>
+									</MetaValue>
+								</li>
+							)}
+						</ul>
+					)}
 					<Text.Base className={styles.description} limitLines={3} overflow={Text.Overflow.Ellipse}>{badge.description}</Text.Base>
 				</div>
 			</div>
